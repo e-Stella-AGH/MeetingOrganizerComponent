680de9bb6ee96ec9fb1c5e572b3488450a6cfd29
Dawid Dębowski <dawid199960@gmail.com>
2021-12-01 21:55:27 +0100

    better message

--- src/components/meetings/HostsList.js
+++ src/components/meetings/HostsList.js
@@ -6,13 +6,15 @@ export const HostsList = ({ hosts, onDelete, center }) => {
     const style = center ? { justifyContent: 'center' } : {}
 
     return(
+      <div>
         <div style={{display: 'flex', flexFlow: 'row wrap', gap: '10px', ...style}}>
             {
                 hosts.length > 0 ?
                     hosts.map(host => (<Chip avatar={<Avatar>{host[0].toUpperCase()}</Avatar>} label={host} color="primary" onDelete={onDelete ? () => onDelete(host) : undefined} />))
                 : <Typography variant="body1" color="textSecondary">Hosts you'll add will appear here. Click tick icon or select to add host.</Typography>
             }
-            <Typography>Remember, you're not added as a host by default!</Typography>
         </div>
+        {!onDelete && <Typography>Remember, you're not added as a host by default!</Typography>}
+      </div>
     )
 }

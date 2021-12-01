f1aa216da9d07c89ef7d7c7f329f16423b96d69d
Dawid Dębowski <dawid199960@gmail.com>
2021-12-01 21:26:32 +0100

    additional warning

--- src/components/meetings/HostsList.js
+++ src/components/meetings/HostsList.js
@@ -7,11 +7,12 @@ export const HostsList = ({ hosts, onDelete, center }) => {
 
     return(
         <div style={{display: 'flex', flexFlow: 'row wrap', gap: '10px', ...style}}>
-            { 
+            {
                 hosts.length > 0 ?
                     hosts.map(host => (<Chip avatar={<Avatar>{host[0].toUpperCase()}</Avatar>} label={host} color="primary" onDelete={onDelete ? () => onDelete(host) : undefined} />))
                 : <Typography variant="body1" color="textSecondary">Hosts you'll add will appear here. Click tick icon or select to add host.</Typography>
             }
+            <Typography>Remember, you're not added as a host by default!</Typography>
         </div>
     )
 }
